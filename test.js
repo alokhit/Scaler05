ec28690dcb8f34e946ee7c9d73133089f42a65f4
alokhit <alokk.hitee@gmail.com>
2024-09-14 12:46:26 +0530

    Test file created

---
+++ test.js
@@ -0,0 +1,9 @@
+var name = "Git Hub Repository";
+console.log(name + " repository " );
+
+
+
+// add/staging --> changes made in repo used to add
+// commit --> Ready to upload on and push on git hub
+//push ---> pusj on git hub
+
