471869dfcf6736cfa0de0f0910eeac8abe54fb9c
alokhit <alokk.hitee@gmail.com>
2024-09-29 23:31:31 +0530

    Object&Method in Js

---
+++ JS/Objects&Methods.js
@@ -0,0 +1,61 @@
+// undefined ==> un defined ==> not defined
+var apples;
+console.log(apples); //undefined
+// declaration => value will be undefined
+
+apples = 10;
+// assigenment  => assigning the value
+
+// For:-
+// Company ABC => job
+
+var cherry;
+console.log(cherry); //undefined
+cherry = "SDE";
+console.log(cherry); //SDE
+
+// null => no value
+// null and 0
+var cookies; //undefined
+cookies = 10;
+cookies = 0;
+//0
+cookies = null;
+// null ==>  falsy value 
+// undefined ==> falsy value 
+
+console.log(null == undefined); //true
+console.log(null === undefined); //false becase of typeof
+
+
+console.log(typeof undefined); //undefined 
+
+console.log(typeof null); //object 
+// The type of null in java script developer design as null
+
+// NOT operator 
+console.log(!null);// true 
+console.log(!undefined);// true 
+// ==> That seems NOT oprator is giving true but undefined and null is falsy value.
+// ==> Hence it prove.
+
+//*** */ undefined will be assigned by JS but NULL is assign by the developers.
+// null/undefined  has to  be given by developer.
+// null  has to  be given by developer
+
+// NULL:-
+// 1. No Value 
+// 2. JS will not assign null value
+// 3. used to represent intentional absence of value 
+
+//For ex:- apple ==> mango can appl tree give
+
+// undefined:-
+// 1. Not assigned 
+// 2. default value that JS assigns
+// 3. check if the variabe has been asssigned a value 
+
+
+
+// ===========>
+    // 7549779949 Rajesh 
